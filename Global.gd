f7816d41774ba244629768d17dac6f1fe63d80d7
John-Cams <143854397+John-Cams@users.noreply.github.com>
2025-10-05 20:19:02 -0700

    Push or smth

--- Global.gd
+++ Global.gd
@@ -6,3 +6,9 @@ var notesToExpect = [-1,-1,-1,-1,-1,-1,-1]
 var requiredInputs = [0,0,0,0]
 var finished = []
 var score = 0
+
+#TODO Implement these do actually work
+var noteLocations = [null,null,null,null]
+var noteInputs = [null, null, null, null]
+var noteCrit = [false, false, false, false]
+var noteComplete = [false, false, false, false]
--- main.gd
+++ main.gd
@@ -6,6 +6,7 @@ signal shuffleSig
 @onready
 var types = [preload("res://assets//Shape.png"),preload("res://assets//Color.png")]
 var inputs = ["redHeart","yellowCircle","greenDiamond","blueTriangle"]
+var colors = ["#FF8888","#FFFF88","#88FF88","8888FF"]
 var currentShape = 0
 var isShape = true
 #TODO Change later
@@ -16,6 +17,25 @@ var activeShapes = []
 var inputsOn = false
 var time = 0
 var notStarted = true
+var timingList = [8, 2, 2, 2, 2]
+
+#Game timeline
+#TODO make a list of every variable
+#TODO make a list of every signal 
+
+
+
+#TODO
+#	Make a different note lists with attributes
+#		Lucky?
+#		Input num?
+#		Location
+#		Useful
+# Figure out how to make custom notes timing
+# BPM
+# If speed = 100 bpm then each note should be move
+
+
 
 
 @export
@@ -49,51 +69,106 @@ func shuffle() :
 	#print(shapes)
 
 func _input(event):
+	if event.is_action_pressed("spaceBar"):
+		$ColorRect.visible = false
+		shuffle()
+	#Check if inputs are on
+	
 	if inputsOn:
+		
 		var inputNum = -1
-		var validInput = false
 		for i in inputs:
-			validInput = event.is_action_pressed(i) or validInput
 			if event.is_action_pressed(i):
 				inputNum = inputs.find(i)
-		if validInput:
-			var locations = [-1,-1,-1,-1]
-			for i in range(Global.notesToExpect.size()):
-				var item = Global.notesToExpect[i]
-				if item != -1:
-					locations[item] = i
+		
+		if inputNum != -1:
+			$StaffArea/CoolColors.color =  colors[inputNum]
+			print(inputNum)
+			#Check if input is needed inputs
+			if inputNum in Global.requiredInputs:
+				
+				#Creates a list of every notes location
+				var locations = [-1,-1,-1,-1]
+				for i in range(Global.notesToExpect.size()):
+					var item = Global.notesToExpect[i]
+					if item != -1:
+						locations[item] = i
 					
-			var relevantNotes = []
-			var theNote = -1
-			var theNoteLocation = -1
-			var relevancy = [0,6,1,5,2,4,3]
-			for j in Global.requiredInputs.size():
-				#print(inputNum == j)
-				if inputNum == Global.requiredInputs[j]:
-					relevantNotes.append(j)
-					#print("input: ",j)
-			#print("relNote: ", relevantNotes)
-			if relevantNotes.size() == 1:
-				if(locations[relevantNotes[0]]):
-					theNote = relevantNotes[0]
-					theNoteLocation = relevancy.find(locations[relevantNotes[0]])
-					#print("single:", theNote)
-			elif relevantNotes.size() != 0:
-				for val in relevancy:
-					for note in relevantNotes:
-						#print("location: ",locations[note])
-						#print("value: ",val)
-						if locations[note] == val:
-							theNote = note
-							theNoteLocation = relevancy.find(locations[relevantNotes[0]])
-							break
-			if(theNote != -1):
-				nextNote(theNote)
-				Global.score += (theNoteLocation+1)*100
-				print(theNoteLocation)
-				print(Global.score)
-				$Score.text = "Score:" + str(Global.score)
+				#Create a list of notes that match the input
+				var importantNotes = []
+				var importantLocations = []
+				for i in Global.requiredInputs.size():
+					if inputNum == Global.requiredInputs[i]:
+						importantNotes.append(inputNum)
+					
+				#Find the item that is the closest to three
+				var theNote = -1
+				for i in [0,6,1,5,2,4,3]:
+					for j in importantNotes.size():
+						if locations[j] == i:
+							theNote = importantNotes[j]
+			
+				if theNote != -1:
+					nextNote(theNote)
+			
+				print(theNote)
+	
+	
+	
+	
+	
+	
+	
+	#if inputsOn:
+		#var inputNum = -1
+		#var validInput = false
+		#
+		##Is input in the list of 
+		#for i in inputs:
+			#validInput = event.is_action_pressed(i) or validInput
+			#if event.is_action_pressed(i):
+				#inputNum = inputs.find(i)
+		#if validInput:
+			#var locations = [-1,-1,-1,-1]
+			#for i in range(Global.notesToExpect.size()):
+				#var item = Global.notesToExpect[i]
+				#if item != -1:
+					#locations[item] = i
+					#
+			#var relevantNotes = []
+			#var theNote = -1
+			#var theNoteLocation = -1
+			#var relevancy = [0,6,1,5,2,4,3]
+			#for j in Global.requiredInputs.size():
+				##print(inputNum == j)
+				#if inputNum == Global.requiredInputs[j]:
+					#relevantNotes.append(j)
+					##print("input: ",j)
+			##print("relNote: ", relevantNotes)
+			#if relevantNotes.size() == 1:
+				#if(locations[relevantNotes[0]]):
+					#theNote = relevantNotes[0]
+					#theNoteLocation = relevancy.find(locations[relevantNotes[0]])
+					##print("single:", theNote)
+			#elif relevantNotes.size() != 0:
+				#for val in relevancy:
+					#for note in relevantNotes:
+						##print("location: ",locations[note])
+						##print("value: ",val)
+						#if locations[note] == val:
+							#theNote = note
+							#theNoteLocation = relevancy.find(locations[relevantNotes[0]])
+							#break
+			#if(theNote != -1) or (locations[theNote] != -1):
+				#
+				#nextNote(theNote)
+				#Global.score += (theNoteLocation+1)*100
 				#print(theNote)
+				#print(theNoteLocation)
+				#print(Global.score)
+				#$Score.text = "Score:" + str(Global.score)
+				##print(theNote)
+				#
 
 ## This function is ran whenever a note becomes "uselss"
 ## that is when the note passes the staff bar or is clicked
@@ -107,21 +182,15 @@ func nextNote(note) :
 	if Global.finished.size() == 4:
 		await $Timer.timeout
 		shuffle()
-		
-			
-func _ready() -> void:
-	shuffle()
-	
+
 func spawnShapes(shapeList):	
 	if(notStarted):
 		notStarted = false
 		var distance = (xStaffMax - xStaffMin) / (shapeList.size())
 		for i in shapeList.size():
 			await $Timer.timeout
-			if!($Music.playing):
-				$Music.play()
 			var new = $Note.duplicate()
-			new.position = Vector2(xStaffMin + (300*i), 200)
+			new.position = Vector2(xStaffMin + (distance*i), 200)
 			var anim_sprite: AnimatedSprite2D = new.get_node("NoteAS")
 			anim_sprite.frame = shapes[i]
 			add_child(new)
@@ -131,7 +200,6 @@ func spawnShapes(shapeList):
 		inputsOn = true	
 	else:
 		await $Spawnstart.timeout
-		print("Wak")
 		notStarted=true
 		spawnShapes(shapeList)
 	
@@ -141,14 +209,11 @@ func spawnShapes(shapeList):
 #func _on_area_2d_entered() -> void:
 	#print("hi")
 
-
 func _on_staff_area_complete_miss() -> void:
 	shuffle()
 
-
 func _on_audio_stream_player_finished() -> void:
 	$Music.play()
-
 
 func _on_timer_timeout() -> void:
 	#print_debug("Hi")
